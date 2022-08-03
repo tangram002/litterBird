4b501a75fb43343f9bf5a2986f66bda173361cd0
tangram002 <tangram002@qq.com>
2022-08-03 10:59:24 +0800

    完成小鸟的初始化界面

---
+++ Assets/Egg Up/Scripts/GameStart.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Suriyun;
+
+public class GameStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public BirdSelectStory[] birds;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("dddddddddddddddddddddddd");
+
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+
+                foreach (var birdd in birds) {
+
+                    birdd.slient();
+                }
+
+                // Debug.Log("碰到对象：" + hit.collider.name + " ta的位置：" + hit.point);
+                ///Debug.DrawLine(ray.origin, hit.point, Color.red);//将射线绘制出来 射线显示红色
+                Debug.Log(hit.collider.gameObject.name);
+
+                BirdSelectStory c = hit.collider.gameObject.GetComponent<BirdSelectStory>();
+
+                c.ByTouch();
+
+
+            }
+            
+        }
+
+    }
+
+    public void startGame() {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+
+    }
+}
---
+++ Assets/Suriyun/Scripts/Bird/BirdSelectStory.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BirdSelectStory : MonoBehaviour
+{
+
+    public Canvas myUI;
+
+    public int storyNum;
+    public Animator animator;
+
+    bool isAction = false;
+
+    public int myAction = 0;
+    public int myTouchAction = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /***
+        switch (storyNum) { 
+        
+            case 0:
+                myAction =1;
+                animator.SetInteger("animation",1);
+                break;
+            case 1:
+                myAction = 2;
+                animator.SetInteger("animation", 2);
+                break;
+            case 2:
+                myAction = 3;
+                animator.SetInteger("animation", 3);
+                break;
+            
+        }
+        */
+        animator.SetInteger("animation", myAction);
+    }
+
+    public void slient() { 
+        
+        myUI.gameObject.SetActive(false);
+        animator.SetInteger("animation", myAction);
+    }
+
+    public void showStory() {
+
+        myUI.gameObject.SetActive(true);
+
+        Vector3 oldSCale = myUI.transform.localScale;
+
+        Vector3 newSCale = Vector3.zero;
+
+        myUI.transform.localScale = newSCale;
+
+        myUI.transform.DOScale(oldSCale,0.5f);
+    }
+    public void ByTouch()
+    {
+
+        if (!isAction)
+        {
+            /**
+            switch (storyNum)
+            {
+
+                case 0:
+                    myTouchAction = 10;           
+                    break;
+                case 1:
+                    myTouchAction = 14;           
+                    break;
+                case 2:
+                    myTouchAction = 17;                    
+                    break;
+            }
+           */
+
+            animator.SetInteger("animation", myTouchAction);
+
+            isAction = true;
+
+            Invoke("reCoverAnimation", 1f);
+        }
+    }
+
+    void reCoverAnimation()
+    {
+        animator.SetInteger("animation", myAction);
+
+        isAction = false;
+
+        BirdSelectStory bs = gameObject.GetComponent<BirdSelectStory>();
+
+        bs.showStory();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
